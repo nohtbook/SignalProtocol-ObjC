a9ff9d376491b1d5da8b8e9cc46cc43289a36d11
Matthew Di Pasquale <liveloveprosper@gmail.com>
2018-02-25 01:46:04 -0500

    Add NS_ASSUME_NONNULL_BEGIN/END where missing

--- Classes/Models/SignalPreKeyMessage_Internal.h
+++ Classes/Models/SignalPreKeyMessage_Internal.h
@@ -1,8 +1,12 @@
 #import "SignalPreKeyMessage.h"
 @import SignalProtocolC;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface SignalPreKeyMessage ()
 
 @property (readonly, nonatomic) pre_key_signal_message *pre_key_signal_message;
 
 @end
+
+NS_ASSUME_NONNULL_END
