293ac7ce9e7c10146f7c6120698b21f9402d5c29
Matthew Di Pasquale <liveloveprosper@gmail.com>
2018-02-25 01:47:41 -0500

    Make NSString properties copy, not strong

--- Classes/Models/SignalAddress.h
+++ Classes/Models/SignalAddress.h
@@ -4,7 +4,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SignalAddress : NSObject
 
-@property (readonly, nonatomic) NSString *name;
+@property (readonly, copy, nonatomic) NSString *name;
 @property (readonly, nonatomic) int32_t deviceId;
 
 - (instancetype)initWithName:(NSString *)name deviceId:(int32_t)deviceId;
